7dbc3564853d933c62d61ce90349eb5f99e3032d
Lalamoffa <lalememmedova1133@gmail.com>
2024-03-28 16:28:15 +0400

    task4

---
+++ ev taps/ev taps/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Services;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ev_taps
+{
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+          
+            Students student1 = new Students("Eli","Eliyev");
+            student1.GroupNumber = "q123";
+            Students.AddStudent(student1);
+            Console.WriteLine(student1.No + "." + student1.Name + "," + student1.Surname);
+
+            Students student2 = new Students("Veli", "Veliyev");
+            student2.GroupNumber = "w45";
+            Students.AddStudent(student2);
+            Console.WriteLine(student2.No + "." + student2.Name + "," + student2.Surname);
+
+            Students student3 = new Students("Aynur", "Eliyeva");
+            student3.GroupNumber = "r678";
+            Students.AddStudent(student3);
+            Console.WriteLine(student3.No + "." + student3.Name + "," + student3.Surname);
+
+            Students student4 = new Students("Lala", "Mammadova");
+            student4.GroupNumber = "t890";
+            Students.AddStudent(student4);
+            Console.WriteLine(student4.No + "." + student4.Name + "," + student4.Surname);
+
+
+            while (true)
+            {
+                Console.WriteLine("1. Search");
+                Console.WriteLine("2. Remove");
+                Console.WriteLine("3. Edit");
+                Console.WriteLine("\n Which operation do you want to select?");
+                int menyu = Convert.ToInt32(Console.ReadLine());
+
+                switch (menyu)
+                {
+                    case 1:
+                       
+                        Students.Search();
+
+                        break;
+
+                    case 2:
+                       Students.RemoveStudent();
+
+                        break;
+
+                    case 3:
+                        Students.Edit();
+                        break;
+
+                    default:
+
+                        Console.WriteLine("Nomre tapilmadi.");
+                        break;
+                       
+                }
+            }
+        }
+    }
+}
---
+++ ev taps/ev taps/Students.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ev_taps
+{
+    internal class Students
+    {
+        public static List<Students> student = new List<Students>();
+        public static int Count;
+        public Students(string ad, string soyad) 
+        {
+            Name = ad;
+            Surname = soyad;
+            Count++;
+            No = Count;
+        }
+
+
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int No { get; set; }
+        public string GroupNumber { get; set; }
+        
+        public static void AddStudent(Students std)
+        {
+            student.Add(std);
+        }
+
+        public static void Search()
+        {
+            Console.WriteLine("Enter a Name:");
+            string name = Console.ReadLine();
+            foreach (var item in student)
+            {
+                if (name == item.Name )
+                {
+                    Console.WriteLine(item.No + "." + item.Name + ", " + item.Surname );
+                    
+                }
+
+            }
+            Console.WriteLine("Information found.");
+        }
+        public static void RemoveStudent()
+        {
+            Console.WriteLine("Enter the number you want to remove:");
+            int number = Convert.ToInt32(Console.ReadLine());
+            foreach (var item in student)
+            {
+                if (number == item.No)
+                {
+                    student.Remove(item);
+                    Console.WriteLine(item.No + "." +item.Name + ", " + item.Surname +  ": Silindi");
+                    break;
+
+                }
+                
+            }
+          
+        }
+        
+
+        public static void Edit()
+        {
+            Console.WriteLine("Enter the number you want to Edit:");
+            int number1 = Convert.ToInt32(Console.ReadLine());
+            foreach (var item in student)
+            {
+                if (number1 == item.No)
+                {
+                    Console.WriteLine("Soyad daxil et.");
+                    item.Surname = Console.ReadLine();                    
+                    Console.WriteLine("Ad daxil et.");
+                    item.Name = Console.ReadLine();
+                    Console.WriteLine("Ad deyisdirildi. Deyisdirilen telebenin melumatlari.");
+                    Console.WriteLine(item.No + "." + item.Name + ", " + item.Surname);
+
+                    return;
+                }
+            }
+            
+        }
+
+
+       
+    }
+}
